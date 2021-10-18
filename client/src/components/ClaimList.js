be70ecfaa34f7b6c020b23a7ad5c84609b6c4ab8
kojima yosuke <yabbee0828@gmail.com>
2021-10-18 22:20:48 +0900

    Scafold ClaimList page

--- client/src/components/ClaimList.js
+++ client/src/components/ClaimList.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import appetito from '../appetito';
+import web3 from '../web3';
 
 class ClaimList extends React.Component {
+  async componentDidMount() {}
+
   render() {
     return (
       <div>
